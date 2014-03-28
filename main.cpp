c0ec2903b0984ee8afa8f2f1d94bf239b603d081
Jonathan van der Steege <jonakeys@hotmail.com>
2014-03-28 01:08:56 +0100

    Added comments to the code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+  TIC TAC TOE
+
+  Created by Jonathan van der Steege (27.03.2014)
+
+  A simple game of 'Tic Tac Toe' to be played by two human players
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,24 +14,25 @@ using namespace std;
 #define cP1 'X';
 #define cP2 'O';
 
-void displayTitle();
-void displayBoard(vector <char>& vBoard);
-int getInput(int& nCount);
-void changeArray(vector <char>& vBoard, int& nCount, int& nLoc);
-bool checkWin(vector <char>& vBoard);
+void displayTitle(); // Display game title
+void displayBoard(vector <char>& vBoard); // Print the board to the screen
+int getInput(int& nCount); // Get user input
+void changeArray(vector <char>& vBoard, int& nCount, int& nLoc); // Use the input to change contents of the array
+bool checkWin(vector <char>& vBoard); // Check if a player has three in a row
 
 int main()
 {
-    int nTurn(0);
+    int nTurn(0); // Counting the turns
     bool GO = true;
-    int aBoard[] = {'1','2','3','4','5','6','7','8','9'};
-    vector <char> vPlayBoard(aBoard,aBoard+sizeof(aBoard)/sizeof(aBoard[0]));
+    int aBoard[] = {'1','2','3','4','5','6','7','8','9'}; // Initializing the gameboard
+    vector <char> vPlayBoard(aBoard,aBoard+sizeof(aBoard)/sizeof(aBoard[0])); // Put the gameboard into a vector
     
     displayTitle();
     displayBoard(vPlayBoard);
 
     while(GO && nTurn < 9) {
 	int nTest = getInput(nTurn);
+	// Check if chosen position is already taken
 	if(vPlayBoard.at(nTest-1)=='X' || vPlayBoard.at(nTest-1)=='O') {
 	    cout << "Locatie " << nTest << " is al bezet." << endl;
 	}
@@ -46,6 +55,7 @@ int main()
 	else speler = 2;
 	cout << "De winnaar is speler " << speler << "!\n";
     }
+    // When 9 turns are gone without a winner, print this:
     else {
 	cout << "Helaas, er is geen winnaar." << endl;
     }
@@ -55,6 +65,7 @@ int main()
     return 0;
 }
 
+// Print the name of the game
 void displayTitle()
 {
     cout << "------------------------------" << endl
@@ -62,6 +73,7 @@ void displayTitle()
 	 << "Speler 1 = X\tSpeler 2 = O" << endl << endl;
 }
 
+// Print the contents of the vector (playing board) to the screen
 void displayBoard(vector <char>& vBoard)
 {
     cout << "\t " << vBoard.at(0) << " | " << vBoard.at(1) << " | " << vBoard.at(2) << endl
@@ -71,6 +83,7 @@ void displayBoard(vector <char>& vBoard)
 	 << "\t " << vBoard.at(6) << " | " << vBoard.at(7) << " | " << vBoard.at(8) << endl << endl;
 }
 
+// Get input from the user and check if it is within valid range (1-9)
 int getInput(int& nCount)
 {
     bool check=false;
@@ -87,6 +100,7 @@ int getInput(int& nCount)
 	}
 	cin >> nChoice;
     
+	// Check if input is one of the locations on the board
 	if(nChoice < 1 || nChoice > 9) {
 	    cout << "Onjuist invoer, probeer het opnieuw. Kies een cijfer tussen 1 en 9." << endl << endl;
 	}
@@ -97,6 +111,11 @@ int getInput(int& nCount)
     return nChoice;
 }
 
+/* Change the contents of the array
+   @param vector <char>& vBoard Pass the playing board
+   @param int& nCount Pass the turncounter
+   @param int& nLoc Pass the chosen location
+ */
 void changeArray(vector <char>& vBoard, int& nCount, int& nLoc)
 {
     if(nCount % 2 == 0) {
@@ -107,6 +126,7 @@ void changeArray(vector <char>& vBoard, int& nCount, int& nLoc)
     }
 }
 
+// Check winning conditions
 bool checkWin(vector <char>& vBoard)
 {
     bool win = false;
